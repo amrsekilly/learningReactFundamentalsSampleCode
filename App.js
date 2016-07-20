203616ba437e8b0329e773f51d587aafd64bb4b5
amrme <amr.e.dev@gmail.com>
2016-07-20 16:14:13 +0200

    learned about stateless funtions

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>
-        Hello world from React!!!!
-      </h1>
-    );
-  }
-}
+const App = () => <p>This is a stateless function!!!!!</p>
 
 export default App;
