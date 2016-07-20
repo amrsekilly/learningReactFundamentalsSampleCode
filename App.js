a45f87fd30d5b68f17eb5d16c8510f43b26863b1
amrme <amr.e.dev@gmail.com>
2016-07-20 16:02:08 +0200

    hello world from react done

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>
+        Hello world from React!!!!
+      </h1>
+    );
+  }
+}
+
+export default App;
