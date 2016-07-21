57dfdfcdb8d323bdaaa38cc2388f117f9933f89d
amrme <amr.e.dev@gmail.com>
2016-07-21 02:14:17 +0200

    learned how to create a default value for the props

--- App.js
+++ App.js
@@ -5,19 +5,23 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     return (
-      <h1>It works, {this.props.user}!</h1>
+      <h1>It works, {this.props.user}{this.props.em}</h1>
     );
   }
 }
 
 App.propTypes = {
-  user: React.PropTypes.string.isRequired
+  user: React.PropTypes.string.isRequired,
+  em: React.PropTypes.string
+}
+
+App.defaultProps = {
+  em: "!"
 }
 
 ReactDOM.render(
-  <App user="Amr" />,
+  <App user="Amr" em="!!!!!!" />,
   document.getElementById('app')
 );
-
 
 export default App;
