2c99854e11f715d435e47fed223f81109ac6e76c
amrme <amr.e.dev@gmail.com>
2016-07-21 02:04:43 +0200

    learned about passing in props from the JSX

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => <p>This is a stateless function!!!!!</p>
+class App extends React.Component {
+  render() {
+    return (
+      <h1>It works, {this.props.user}!</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App user="Amr" />,
+  document.getElementById('app')
+);
+
 
 export default App;
