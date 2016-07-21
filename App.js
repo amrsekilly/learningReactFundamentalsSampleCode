cfe3ec27d51adfea7f4abf492aa83ca2bdb6bd5a
amrme <amr.e.dev@gmail.com>
2016-07-21 08:54:55 +0200

    understood how to render a component inside a component and what happens to the rendered view in terms if component lifecycle

--- App.js
+++ App.js
@@ -7,45 +7,59 @@ class App extends React.Component {
   // create a state
   constructor() {
     super();
-
-    this.state = {number: 0};
   }
 
   mount() {
-    //console.log("mounting");
+    ReactDOM.render(
+      <SomeText />,
+      document.getElementById('txtHere')
+    );
   }
 
   unmount() {
-    ReactDOM.unmountComponentAtNode(this.refs.txt);
+    ReactDOM.unmountComponentAtNode(document.getElementById('txtHere'));
   }
 
-  countOn() {
-    this.setState({number: this.state.number+3})
-  }
 
-  componentWillMount() {
-    console.log("will mount");
-  }
 
   render() {
-    console.log("component rendering");
+
     return (
       <div>
-        <button onClick={this.countOn.bind(this)}>Mount</button>
+        <button onClick={this.mount.bind(this)}>Mount</button>
         <br />
         <br />
         <button onClick={this.unmount.bind(this)}>Unmount</button>
-        <br />
-        <br />
-        <h2 ref="txt">Some text {this.state.number}</h2>
+        <div id="txtHere"></div>
       </div>
     );
+  }
+}
+
+class SomeText extends React.Component {
+
+  componentWillMount() {
+    console.log("will mount");
   }
 
   componentDidMount() {
     console.log("component mounted");
   }
 
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
+  render() {
+    console.log("component rendering");
+    return (
+      <div>
+        <br />
+        <br />
+        <h2>Some text</h2>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
